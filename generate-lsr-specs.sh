ac32dbd2a047a2bb5081ac57f952a3b8df2f6576
Sergei Petrosian <spetrosi@redhat.com>
2024-03-20 10:53:30 +0100

    Rename ansible-windows-var.sh to ansible-windows-vars.sh for consistency

--- generate-lsr-specs.sh
+++ generate-lsr-specs.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -x
 
-for vars_file in ansible-posix-vars.sh ansible-windows-var.sh microsoft-ad-vars.sh; do
+for vars_file in ansible-posix-vars.sh ansible-windows-vars.sh microsoft-ad-vars.sh; do
     source ./"${vars_file}"
     sh generate-spec.sh
 done
